04d7ff8b31bc38f99faf145c4ab4651bd13b5b93
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-08-18 15:38:39 +0200

    added tests for type builtin

--- tests_scripts/test_builtins_2.sh
+++ tests_scripts/test_builtins_2.sh
@@ -6,7 +6,7 @@
 #    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/06 16:09:28 by jmartel           #+#    #+#              #
-#    Updated: 2019/08/17 12:39:15 by jmartel          ###   ########.fr        #
+#    Updated: 2019/08/18 11:50:13 by jmartel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,10 @@ launch "Builtins (2)"
 
 	launch "Simple tests"
 	test_launch 'type ; echo $?'
+	test_launch 'type [['
+	test_launch 'type { }'
+	test_launch 'type ]]'
+	test_launch 'type ]'
 	test_launch 'type -- ; echo $?'
 	test_launch 'type -a -- ls; echo $?'
 	test_launch 'type -a -- bash; echo $?'
@@ -26,30 +30,30 @@ launch "Builtins (2)"
 	test_launch 'type ls cd brew type'
 	test_launch 'type ls cd brew none type'
 	test_launch 'type ls cd brew type none'
-	test_launch 'ls ; type ls cd brew type'
-	test_launch 'ls ; type ls cd none brew type'
-	test_launch 'ls' 'type ls cd brew type'
-	test_launch 'ls' 'type ls cd brew none type'
+	test_launch 'ls ; type ls cd brew type while'
+	test_launch 'ls ; type ls cd none brew type while'
+	test_launch 'ls' 'type ls cd brew type while'
+	test_launch 'ls' 'type ls cd brew none type while'
 
-	# test_launch 'type -aa cd ls brew while type'
-	# test_launch 'type -p ls brew while type'
-	# test_launch 'type -t cd ls brew while type'
-	# test_launch 'type -a -p -t -a cd ls brew while type'
-	# test_launch 'type -a -- p cd ls brew while type'
-	# test_launch 'type -a -p -- -t cd ls brew while type'
-	# test_launch 'type --t --a cd ls brew while type'
-	# test_launch 'type -t -a- cd ls brew while type'
-	# test_launch 'type -a cd ls brew while type -a'
-	# test_launch 'type cd ls brew while type'
-	test_launch 'type -p -t cd ls brew  type'
-	test_launch 'type -t -p cd ls brew  type'
-	test_launch 'type -a -p -t cd ls brew  type'
-	test_launch 'type -a -t -p cd ls brew type'
-	test_launch 'type -p -t -a cd ls brew  type'
-	test_launch 'type -t -p -a cd ls brew  type'
-	test_launch 'ls' 'type -p -t cd ls brew  type'
-	test_launch 'ls' 'type -t -p cd ls brew  type'
-	test_launch 'ls' 'type -a -p -t cd ls brew  type'
-	test_launch 'ls' 'type -a -t -p cd ls brewe type'
+	test_launch 'type -aa cd ls brew while type'
+	test_launch 'type -p ls brew while type'
+	test_launch 'type -t cd ls brew while type'
+	test_launch 'type -a -p -t -a cd ls brew while type'
+	test_launch 'type -a -- p cd ls brew while type'
+	test_launch 'type -a -p -- -t cd ls brew while type'
+	test_launch 'type --t --a cd ls brew while type'
+	test_launch 'type -t -a- cd ls brew while type'
+	test_launch 'type -a cd ls brew while type -a'
+	test_launch 'type cd ls brew while type'
+	test_launch 'type -p -t cd ls brew  type while'
+	test_launch 'type -t -p cd ls brew  type while'
+	test_launch 'type -a -p -t cd ls brew  type while'
+	test_launch 'type -a -t -p cd ls brew type while'
+	test_launch 'type -p -t -a cd ls brew  type while'
+	test_launch 'type -t -p -a cd ls brew  type while'
+	test_launch 'ls' 'type -p -t cd ls brew  type while'
+	test_launch 'ls' 'type -t -p cd ls brew  type while'
+	test_launch 'ls' 'type -a -p -t cd ls brew  type while'
+	test_launch 'ls' 'type -a -t -p cd ls brewe type while'
 
 finish
