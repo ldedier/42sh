e3e472927e96b6fcc22ed0a3e6fcfed16a11e7fc
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-09-12 00:09:29 +0200

    fusionned cmd name and cmd suffix

--- srcs/traverse/sh_traverse_cmd_name.c
+++ srcs/traverse/sh_traverse_cmd_name.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 17:31:30 by ldedier           #+#    #+#             */
-/*   Updated: 2019/09/04 19:18:31 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/09/11 20:17:12 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,5 @@
 
 int			sh_traverse_cmd_name(t_ast_node *node, t_context *context)
 {
-	t_ast_node	*child;
-	int			ret;
-
-	if (context->phase == E_TRAVERSE_PHASE_EXPANSIONS)
-	{
-		context->current_command_node = node;
-		child = (t_ast_node *)node->children->content;
-		if (child && child->token)
-		{
-			ret = sh_expansions(context, child);
-			context->current_command_node = NULL;
-			return (ret);
-		}
-		return (SUCCESS);
-	}
-	else if (context->phase == E_TRAVERSE_PHASE_EXECUTE)
-	{
-		child = (t_ast_node *)node->children->content;
-		if (child == NULL)
-			return (SUCCESS);
-		if (!*child->token->value && child->token->expansion)
-			return (SUCCESS);
-		if (ft_dy_tab_add_str(context->params, child->token->value))
-			return (sh_perror(SH_ERR1_MALLOC, "sh_traverse_cmd_name"));
-		return (SUCCESS);
-	}
-	else
-		return (SUCCESS);
+	return (sh_traverse_cmd_suffix(node, context));
 }
--- srcs/traverse/sh_traverse_cmd_suffix.c
+++ srcs/traverse/sh_traverse_cmd_suffix.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 17:31:30 by ldedier           #+#    #+#             */
-/*   Updated: 2019/08/28 11:29:36 by jdugoudr         ###   ########.fr       */
+/*   Updated: 2019/09/11 20:17:36 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,58 +36,23 @@ static int	sh_process_traverse_cmd_suffix(
 	return (SUCCESS);
 }
 
-/* JMARTEL TO UNCOMMENT AND AKNOWLEDGE (then delete)
-int		test_field_splitting_function(t_ast_node *child, t_list **ptr)
-{
-	int			i;
-	t_ast_node	*node;
-
-	*ptr = (*ptr)->next; //pour passer au suivant dans la liste pas encore modifiee
-	node = child;
-	i = 0;
-	while (i < 3)
-	{
-		if (!(node = sh_add_word_to_ast(node,
-			ft_strdup("je remplace 3 fois mdr le 2 eme param hehe"))))
-		{
-			return (1);
-		}
-		i++;
-	}
-	sh_delete_node_from_parent(child);
-	return (0);
-}
-*/
-
 int			sh_traverse_cmd_suffix(t_ast_node *node, t_context *context)
 {
 	t_ast_node	*child;
 	t_list		*ptr;
 	int			ret;
-/* JMARTEL TO UNCOMMENT AND AKNOWLEDGE
-	int			i;
+	t_list		*next;
 
-	i = 1;
-*/
 	ptr = node->children;
 	ret = SUCCESS;
 	sh_traverse_tools_show_traverse_start(node, context);
 	while (ptr != NULL)
 	{
 		child = (t_ast_node *)ptr->content;
+		next = ptr->next;
 		if (((ret = sh_process_traverse_cmd_suffix(child, context)) != SUCCESS))
 			break ;
-		/* JMARTEL TO UNCOMMENT AND AKNOWLEDGE
-		if (i == 2 && context->phase == E_TRAVERSE_PHASE_REDIRECTIONS)
-		{
-			test_field_splitting_function(child, &ptr);
-			sh_print_ast(context->shell->parser.ast_root, 0);
-			if (!ptr) //au cas ou on a remplace le dernier token
-				break ;
-		}
-		i++;
-		*/
-		ptr = ptr->next;
+		ptr = next;
 	}
 	sh_traverse_tools_show_traverse_ret_value(node, context, ret);
 	return (ret);
