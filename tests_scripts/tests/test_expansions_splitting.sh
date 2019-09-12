68e554d402d8d9009476cbadbe2b8d020424f756
Jeremie MARTEL <jmartel@e1r2p7.42.fr>
2019-09-12 09:51:03 +0200

    added tests for field splitting, found a infinite loop, need to finish completing tests_files/splitting

--- tests_scripts/tests/test_expansions_splitting.sh
+++ tests_scripts/tests/test_expansions_splitting.sh
@@ -6,7 +6,7 @@
 #    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/06 16:09:28 by jmartel           #+#    #+#              #
-#    Updated: 2019/09/04 11:54:12 by jmartel          ###   ########.fr        #
+#    Updated: 2019/09/12 09:44:12 by jmartel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,5 +16,22 @@ launch "Field splitting"
 		test_launch 'var="-l -A"' 'ls $var'
 		test_launch 'var="ls -l"' '$var -A'
 		test_launch 'var="ls -l -A"' '$var'
+		test_launch 'IFS="-"' 'var="ls -l -A"' '$var'
+		test_launch 'IFS="- "' 'var="ls -l -A"' '$var'
+		test_launch 'var="Okalmos     Specul     os"' 'echo $var'
+		test_launch 'IFS="A "' 'var="OkAlm   SpeculAs"' 'echo $var'
+		test_launch "var='\"Okalmos   Speculos\"'" 'echo $var'
+		test_launch 'IFS=AAAAAA''var=AAAAA' 'echo $var'
+		test_launch 'IFS=A''var=AAAAA' 'echo $var'
+		test_launch 'IFS=ABC''var=BAC' 'echo $var'
+		test_launch 'IFS=ABC''var=B   A      C' 'echo $var'
+
+	launch "hard"
+		for i in `seq 1 5` ; do
+			test_launch_pipe ./tests_files/splitting/splitting_${i}
+		done
+
+
+# TESTS with specialchars (quotes, ...) in IFS var
 
 finish
