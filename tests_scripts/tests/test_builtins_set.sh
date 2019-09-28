983660bc843d110dd299456158dd156b164dc9a3
Jeremie MARTEL <jmartel@e1r2p17.42.fr>
2019-09-28 23:34:33 +0200

    fixed errors in launch 'name' in tests and added tests for environnment builtins

--- tests_scripts/tests/test_builtins_set.sh
+++ tests_scripts/tests/test_builtins_set.sh
@@ -14,10 +14,18 @@ launch "set"
 	test_launch "set 1>&-"
 
 	## Manual tests
-test_launch "var=tamer" "set | grep var="
-test_launch "var=tamer set | grep var="
-test_launch "HOME=/ set | grep HOME="
-test_launch "HOME=/ | set | grep HOME="
-test_launch "HOME=/" "HOME=Okalm | set | grep HOME="
-test_launch "var=Okalmos" "var=tamer set | grep HOME="
-test_launch "var=Okalmos" "var=tamer | set | grep HOME="
+	test_launch "var=tamer" "set | grep var="
+	test_launch "var=tamer set | grep var="
+	test_launch "HOME=/ set | grep HOME="
+	test_launch "HOME=/ | set | grep HOME="
+	test_launch "HOME=/" "HOME=Okalm | set | grep HOME="
+	test_launch "var=Okalmos" "var=tamer set | grep HOME="
+	test_launch "var=Okalmos" "var=tamer | set | grep HOME="
+
+	test_launch 'export okalm' 'export okalm' 'set | grep okalm' 'export | grep okalm'
+	test_launch 'var=Tamer' 'set | grep var=Tamer' 'export grep var=Tamer'
+	test_launch 'variable=' 'set | grep variable=' 'export grep variable='
+	test_launch 'set | grep HOME' 'eport | grep HOME'
+	test_launch 'unset HOME' 'set | grep HOME' 'eport | grep HOME'
+	test_launch 'variable=' 'set | grep variable= ; export grep variable=' 'unset variable' 'set | grep variable= ; export grep variable='
+	test_launch 'export variable=' 'set | grep variable= ; export grep variable=' 'unset variable' 'set | grep variable= ; export grep variable='
