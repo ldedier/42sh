c1edeede975ee99a75dd5c35aeeaf965d556fc72
Jeremie MARTEL <jmartel@e2r2p16.42.fr>
2019-10-05 07:40:01 +0200

    continue fixing dystr in libft

--- libft/dystr/ft_dy_str_add_index.c
+++ libft/dystr/ft_dy_str_add_index.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:19:55 by ldedier           #+#    #+#             */
-/*   Updated: 2019/10/05 06:32:30 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/10/05 07:33:56 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_dy_str_add_index(t_dy_str *d_str, char c, size_t index)
 {
 	int i;
 
-	while (d_str->current_size >= d_str->max_size)
+	while (d_str->current_size + 1 >= d_str->max_size)
 	{
 		if (ft_dy_str_realloc(d_str))
 			return (1);
