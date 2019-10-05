c936448d4e23cbb89df8422e3576f859f8d9974d
Jeremie MARTEL <jmartel@e2r2p16.42.fr>
2019-10-05 07:25:03 +0200

    fixed bugs in dy_str functions in libft

--- libft/dystr/ft_dy_str_replace.c
+++ libft/dystr/ft_dy_str_replace.c
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_dy_str_add_index.c                              :+:      :+:    :+:   */
+/*   ft_dy_str_replace.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 16:19:55 by ldedier           #+#    #+#             */
-/*   Updated: 2019/10/05 06:32:30 by jmartel          ###   ########.fr       */
+/*   Created: 2019/10/05 06:45:43 by jmartel           #+#    #+#             */
+/*   Updated: 2019/10/05 06:48:52 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_dy_str_add_index(t_dy_str *d_str, char c, size_t index)
+int		ft_dy_str_replace(t_dy_str *dy_str, char *str)
 {
-	int i;
+	size_t		len;
 
-	while (d_str->current_size >= d_str->max_size)
-	{
-		if (ft_dy_str_realloc(d_str))
-			return (1);
-	}
-	i = d_str->current_size;
-	while ((size_t)i > index)
-	{
-		d_str->str[i] = d_str->str[i - 1];
-		i--;
-	}
-	d_str->str[i] = c;
-	d_str->str[++d_str->current_size] = 0;
+	if (dy_str->str)
+		ft_strdel(&(dy_str->str));
+	if (!(dy_str->str = ft_strdup(str)))
+		return (1);
+	len = ft_strlen(str);
+	dy_str->current_size = len;
+	dy_str->max_size = len;
 	return (0);
 }
+
