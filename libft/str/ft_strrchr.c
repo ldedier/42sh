0ab1cdc94224e35bfbdb2251329991d5ff370bf2
Jeremie MARTEL <jmartel@e2r2p16.42.fr>
2019-10-07 06:42:24 +0200

    fixed invalid read in ft_strrchr ><'

--- libft/str/ft_strrchr.c
+++ libft/str/ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 12:01:50 by jmartel           #+#    #+#             */
-/*   Updated: 2018/11/08 12:01:51 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/10/07 06:40:27 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strrchr(const char *s, int c)
 		return ((char*)(s + i));
 	else
 		i--;
-	while (i >= 0 && s[i] != (char)c)
+	while (i > 0 && s[i] != (char)c)
 		i--;
 	if (s[i] == (char)c)
 		return ((char*)(s + i));
