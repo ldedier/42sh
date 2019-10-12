7f60dfc168a2582704de480828d12600f50be3ba
Ludovic DEDIER <ldedier@e2r6p9.42.fr>
2019-10-12 18:49:37 +0200

    wip architecture

--- srcs/expansions/sh_expansions_pattern_matching.c
+++ srcs/expansions/sh_expansions_pattern_matching.c
@@ -6,24 +6,30 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 13:31:28 by ldedier           #+#    #+#             */
-/*   Updated: 2019/10/12 13:31:28 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/10/12 18:44:15 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_21.h"
 
+int			fill_regexp(t_list **value, char *str)
+{
+
+	return (0);
+}
+
 /*
-** Le pattern matching (globing) : *, ?, [], !
-** et les intervals de caractères avec -
+** pattern matching (globing) : *, ?, [], !, intervals with '-'
 */
 
-int			sh_expansions_pattern_matching(t_context *context,
-				t_ast_node *node)
+int			sh_expansions_pattern_matching(t_context *context, t_ast_node *node)
 {
 	t_ast_node	*child;
 	char		*str;
+	t_list		*matches;
 
 	str = node->token->value;
+	matches = NULL;
 //	ft_printf("value: %s\n", str);
 	(void)context;
 	(void)node;
