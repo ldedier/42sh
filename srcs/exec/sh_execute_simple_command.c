affc265fb8ff4aca8d2c5dc2ac80af0f7b6114e5
Julien DUGOUDRON <jdugoudr@e2r5p4.42.fr>
2019-10-31 17:40:50 +0100

    Just move the search path command function calls before to fork. The enable the hash adding. Error message are still display after the fork to enable redirection.

--- srcs/exec/sh_execute_simple_command.c
+++ srcs/exec/sh_execute_simple_command.c
@@ -6,12 +6,29 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/28 17:31:19 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/10/30 22:58:08 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/10/31 17:37:11 by jdugoudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_21.h"
 #include "sh_job_control.h"
+
+static int		sh_no_slash_cmd(t_context *context)
+{
+	if (sh_traverse_sc_search_in_hash(context) != SUCCESS)
+	{
+		if (sh_traverse_sc_search_in_path(context) == FAILURE)
+			return (FAILURE);
+	}
+	return (SUCCESS);
+}
+
+static int		sh_slash_cmd(t_context *context)
+{
+	if (!(context->path = ft_strdup(context->params->tbl[0])))
+		return (sh_perror(SH_ERR1_MALLOC, "traverse_simple_command"));
+	return (SUCCESS);
+}
 
 int 		sh_execute_simple_command(t_ast_node *father_node, t_context *context)
 {
@@ -25,7 +42,17 @@ int 		sh_execute_simple_command(t_ast_node *father_node, t_context *context)
 		if ((context->builtin = sh_builtin_find(context)))
 			ret = sh_execute_builtin(father_node, context);
 		else
+		{
+			if (!ft_strchr(context->params->tbl[0], '/'))
+			{
+				ret = sh_no_slash_cmd(context);
+				if (ret != SUCCESS)
+					return (ret);
+			}
+			else if (sh_slash_cmd(context) == FAILURE)
+					return (FAILURE);
 			ret = sh_execute_binary(father_node, context);
+		}
 	}
 	return (ret);
 }
