63dd09a5990763e23864ff93c150c7e8aaba727d
Jeremie MARTEL <jmartel@e1r13p7.42.fr>
2019-11-07 00:28:04 +0100

    added tests for globbing, especially for permissions

--- tests_scripts/tests/test_globbing.sh
+++ tests_scripts/tests/test_globbing.sh
@@ -6,7 +6,7 @@
 #    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/16 05:23:32 by jmartel           #+#    #+#              #
-#    Updated: 2019/10/23 04:10:37 by jmartel          ###   ########.fr        #
+#    Updated: 2019/11/07 00:23:41 by jmartel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -88,6 +88,7 @@ launch "Globbing"
 	test_launch 'cd sandbox' 'echo ../**/../**' 'echo [a]*[b] ; echo $?' 'echo [!a]*[!b] ; echo $?' 'echo ?*? ; echo $?'
 	test_launch 'cd sandbox' 'echo */*/' 'echo */*'
 	test_launch 'cd sandbox' 'echo /*/*' 'echo /*/*/'
+	test_launch 'cd sandbox' 'echo /*' 'echo /*/'
 
 	launch_show "question mark"
 	test_launch 'cd sandbox' 'ls file?' 'ls file?*'
@@ -110,25 +111,43 @@ launch "Globbing"
 	test_launch 'cd sandbox' 'echo file\[!1-2]'
 	test_launch 'cd sandbox' 'echo file"[!2-3]"'
 	test_launch 'cd sandbox' 'echo .*/"a"* '
-	
-	## rm -rf sandbox
+	test_launch 'echo /dev/*'
+
+	launch_show "Permissions"
+	test_launch 'cd sandbox' 'touch permission ; chmod 700 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+	test_launch 'cd sandbox' 'touch permission ; chmod 600 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+	test_launch 'cd sandbox' 'touch permission ; chmod 500 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+	test_launch 'cd sandbox' 'touch permission ; chmod 400 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+	test_launch 'cd sandbox' 'touch permission ; chmod 300 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+	test_launch 'cd sandbox' 'touch permission ; chmod 200 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+	test_launch 'cd sandbox' 'touch permission ; chmod 100 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'chmod 777 permission ; rm permission'
+
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 700 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 600 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 500 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 400 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 300 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 200 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+	test_launch 'cd sandbox' 'mkdir permission ; chmod 100 permission' 'echo ?????????? ; echo $?' 'echo ??????????/ ; echo $?' 'echo perm* ; echo $?' 'echo perm*/ ; echo $?' 'chmod 777 permission ; rm -r permission'
+
+	rm -rf sandbox
 
 	launch_show "Correction"
 	test_launch 'mkdir -p empty ; cd empty' 'touch a? b* [c]; ls -1'
 	test_launch 'cd empty' 'echo a > aa && echo b > ab && cat -e a?'
-	# test_launch 'cd empty' 'rm -f *' 'ls'
+	test_launch 'cd empty' 'rm -f *' 'ls'
 	test_launch 'cd empty' 'mkdir -p one/two'
 	test_launch 'cd empty' 'echo "four" > one/two/three'
 	test_launch 'cd empty' 'echo "four" > one/five'
 	test_launch 'cd empty' 'cat ./*/*/* ./*/*'
 	test_launch 'cd empty' 'ls /??r'
-	# test_launch 'cd empty' 'rm -rf ./*' 'ls'
-	test_launch 'cd empty' 'mkdir "[x" && touch "[x/foo"'
+	test_launch 'cd empty' 'rm -rf ./*' 'ls'
+	test_launch 'cd empty' 'mkdir -p "[x"' 'touch "[x/foo"'
 	test_launch 'cd empty' 'echo [*; echo *[x; echo [x/*'
-	# test_launch 'cd empty' 'rm -rf *'
+	test_launch 'cd empty' 'rm -rf *'
 	test_launch 'cd empty' 'touch "a[a-z][x" "ab[x"'
 	test_launch 'cd empty' 'echo a[a-z][*; echo a[a-z]*; echo a[a\-z]*'
-	# test_launch 'cd empty' 'rm -f *'
+	test_launch 'cd empty' 'rm -f *'
 	test_launch 'cd empty' 'touch ".bc" "abc" "bbc" "cbc" "-bc" \!bc "+bc" ",bc" "0bc" "1bc"'
 	test_launch 'cd empty' 'echo [ab-]*'
 	test_launch 'cd empty' 'echo [-ab]*'
@@ -139,11 +158,12 @@ launch "Globbing"
 	test_launch 'cd empty' 'echo [--1]*'
 	test_launch 'cd empty' 'echo [z-a]*'
 	test_launch 'cd empty' 'echo [a-a]bc'
-	# test_launch 'cd empty' 'rm -f * .*; ls -A'
+	test_launch 'cd empty' 'rm -f * .*; ls -A'
 	test_launch 'cd empty' 'touch aa bb cc 11 22 33'
 	test_launch 'cd empty' 'echo **'
 	test_launch 'cd empty' 'echo [!a-z]'
 	test_launch 'cd empty' 'echo [4-z][A-b]'
-	# test_launch 'rm -rf empty'
+	
+	rm -rf empty
 
 finish
