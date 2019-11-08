75ed20d5a10aedcdeb5bbe0a97916c70f2f1d232
Jeremie MARTEL <jmartel@e1r1p9.42.fr>
2019-11-08 06:58:58 +0100

    fixed the fixer

--- tests_scripts/start.sh
+++ tests_scripts/start.sh
@@ -75,7 +75,7 @@ for arg in $@ ; do
 done
 
 make -C $path && cp "${path}/${exec}" . || exit
-if [ -n "$valgrind" ] ; then cp -R ${path}/${exec}.dSYM .
+if [ -n "$valgrind" ] ; then cp -R ${path}/${exec}.dSYM . ; fi
 
 source ${src_dir}/functions.sh
 
