cad12e9bbc0f974b902935c1acc1d6f68ee147e2
Jeremie MARTEL <jmartel@e1r2p17.42.fr>
2019-11-19 07:57:14 +0100

    fixed small error in lexer, some tokens were added as undefined

--- srcs/arithmetic/lexer/sh_ar_lexer_rule_5.c
+++ srcs/arithmetic/lexer/sh_ar_lexer_rule_5.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 19:43:58 by jmartel           #+#    #+#             */
-/*   Updated: 2019/11/19 06:46:24 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/11/19 06:55:42 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,7 @@ int			sh_ar_lexer_rule_5(t_lexer *lexer)
 	{
 		if (lexer->expansion == lexer->c)
 		{
+			rule5_set_token_id_one_len(lexer, &lexer->c);
 			lexer->tok_len += 1;
 			t_ar_lexer_add_token(lexer);
 			return (LEX_OK);
