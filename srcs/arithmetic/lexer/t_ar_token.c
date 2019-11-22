ee04a2a56958daada8168e75067eecbe3b12afd5
daoudm <marwadaoud1993@gmail.com>
2019-11-22 21:51:21 +0100

    Had to cast the enum because of a linux flag

--- srcs/arithmetic/lexer/t_ar_token.c
+++ srcs/arithmetic/lexer/t_ar_token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   t_ar_token.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 21:12:41 by jmartel           #+#    #+#             */
-/*   Updated: 2019/11/22 13:37:39 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/11/22 21:10:04 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,45 @@
 
 void	t_ar_token_show_1(t_token *token)
 {
-	if (token->id == LEX_TOK_AR_GREATEQ)
+	if (token->id == (t_symbol_id)LEX_TOK_AR_GREATEQ)
 		ft_dprintf(2, ">=");
-	else if (token->id == LEX_TOK_AR_EQUAL)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_EQUAL)
 		ft_dprintf(2, "==");
-	else if (token->id == LEX_TOK_AR_NOT_EQUAL)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_NOT_EQUAL)
 		ft_dprintf(2, "!=");
-	else if (token->id == LEX_TOK_AR_INTEGER)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_INTEGER)
 		ft_dprintf(2, "INT : %ld", token->lval);
-	else if (token->id == LEX_TOK_AR_VARIABLE)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_VARIABLE)
 		ft_dprintf(2, "VAR : %s", token->value);
-	else if (token->id == LEX_TOK_AR_INC)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_INC)
 		ft_dprintf(2, "++");
-	else if (token->id == LEX_TOK_AR_DEC)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_DEC)
 		ft_dprintf(2, "--");
-	else if (token->id == END_OF_INPUT_AR)
+	else if (token->id == (t_symbol_id)END_OF_INPUT_AR)
 		ft_dprintf(2, "EOI");
 }
 
 void	t_ar_token_show(t_token *token)
 {
-	if (token->id == LEX_TOK_AR_AND)
+	if (token->id == (t_symbol_id)LEX_TOK_AR_AND)
 		ft_dprintf(2, "&&");
-	else if (token->id == LEX_TOK_AR_OR)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_OR)
 		ft_dprintf(2, "||");
-	else if (token->id == LEX_TOK_AR_PLUS)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_PLUS)
 		ft_dprintf(2, "+");
-	else if (token->id == LEX_TOK_AR_MINUS)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_MINUS)
 		ft_dprintf(2, "-");
-	else if (token->id == LEX_TOK_AR_MULTIPLY)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_MULTIPLY)
 		ft_dprintf(2, "*");
-	else if (token->id == LEX_TOK_AR_DIVIDE)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_DIVIDE)
 		ft_dprintf(2, "/");
-	else if (token->id == LEX_TOK_AR_MODULO)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_MODULO)
 		ft_dprintf(2, "%%");
-	else if (token->id == LEX_TOK_AR_LESS)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_LESS)
 		ft_dprintf(2, "<");
-	else if (token->id == LEX_TOK_AR_GREAT)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_GREAT)
 		ft_dprintf(2, ">");
-	else if (token->id == LEX_TOK_AR_LESSEQ)
+	else if (token->id == (t_symbol_id)LEX_TOK_AR_LESSEQ)
 		ft_dprintf(2, "<=");
 	else
 		t_ar_token_show_1(token);
