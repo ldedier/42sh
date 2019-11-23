b38f67832d5392f154611393f2e120d41c71d8b1
Jeremie MARTEL <jmartel@e1r2p12.42.fr>
2019-11-23 18:19:51 +0100

    fixed master :/

--- srcs/globbing/sh_globbing.c
+++ srcs/globbing/sh_globbing.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 13:31:28 by ldedier           #+#    #+#             */
-/*   Updated: 2019/11/23 17:57:55 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/11/23 18:18:50 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,23 +55,7 @@ static int	sh_expansions_globbing_matches(
 	}
 	ft_lstdel(&start, NULL);
 	ft_dy_tab_empty(quotes);
-	if (sh_verbose_expansion())
-		sh_print_ast_root(node, g_glob.cfg);
 	return (SUCCESS);
-}
-
-void		t_regexp_show_tab(t_dy_tab *regexp_tab)
-{
-	t_list	**tbl;
-	int		i;
-
-	i = 0;
-	tbl = (t_list**)regexp_tab->tbl;
-	while (tbl[i])
-	{
-		t_regexp_show_list(tbl[i]);
-		i++;
-	}
 }
 
 static void	clean_non_original_quotes(t_dy_tab *quotes)
@@ -109,7 +93,8 @@ static void	clean_non_original_quotes(t_dy_tab *quotes)
 **	SUCCESS : returned even if no globbinghad been processed, for any reasons
 */
 
-static int	sh_expansions_globbing_process_node(t_ast_node *node, t_dy_tab *quotes)
+static int	sh_expansions_globbing_process_node(
+	t_ast_node *node, t_dy_tab *quotes)
 {
 	char		*str;
 	t_list		*matches;
@@ -124,17 +109,13 @@ static int	sh_expansions_globbing_process_node(t_ast_node *node, t_dy_tab *quote
 	if (!ft_strpbrk(str, "?[*"))
 		return (SUCCESS);
 	if ((ret = sh_glob_lexer(str, &regexp_tab, quotes)) == FAILURE)
-		return (ret);
+		return (FAILURE);
 	else if (ret == ERROR)
 		return (SUCCESS);
-	if (sh_verbose_globbing())
-		t_regexp_show_tab(regexp_tab);
 	init_path(&path, str);
 	ret = sh_expansions_pattern_matching(
 		path, (t_list**)regexp_tab->tbl, &matches);
-	if (ret == FAILURE)
-		return (FAILURE);
-	if (matches)
+	if (!ret && matches)
 		ret = sh_expansions_globbing_matches(node, matches, quotes);
 	t_regexp_free_tab(&regexp_tab);
 	return (ret);
@@ -150,7 +131,7 @@ int			sh_expansions_globbing(t_ast_node *node, t_dy_tab *quotes)
 	head = node->parent->children;
 	ret = SUCCESS;
 	children = (t_ast_node*)head->content;
-	if (children->token->lval == -1)
+	if (!children || !children->token || children->token->lval == -1)
 		return (SUCCESS);
 	while (head && !ret)
 	{
@@ -161,7 +142,7 @@ int			sh_expansions_globbing(t_ast_node *node, t_dy_tab *quotes)
 		}
 		children = (t_ast_node*)head->content;
 		next_head = head->next;
-		if (children->token->lval != -1)
+		if (children && children->token && children->token->lval != -1)
 			ret = sh_expansions_globbing_process_node(children, quotes);
 		head = next_head;
 	}
